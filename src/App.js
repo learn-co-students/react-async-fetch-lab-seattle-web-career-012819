72d66a674b7c4c76a366f326a12fb08b4a75a7aa
Ezra Schwepker <ezraschwepker@gmail.com>
2019-04-04 15:54:09 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,34 @@
-// create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  constructor (props) {
+    super(props)
+
+
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(console.log)
+  }
+
+  render() {
+    return <div></div>
+  }
+}
+
+function httpRequest(url, method='GET', data={}) {
+  const init = {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    method: method,
+    body: JSON.stringify(data)
+  }
+  if (method.toLowerCase() === 'get') delete init.body;
+  else if (method.toLowerCase() === 'post' && init.body.id) delete init.body.id;
+
+  return fetch( url, init);
+}
